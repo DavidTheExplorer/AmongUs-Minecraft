e3561a01d4d16178a6954b56eaefa9732b8a2d40
David <darknight4123@gmail.com>
2021-05-03 16:03:46 +0300

    Finished the wires task, tweaked TaskInventoryManager

--- src/dte/amongus/shiptasks/list/wires/WiresInventoryManager.java
+++ src/dte/amongus/shiptasks/list/wires/WiresInventoryManager.java
@@ -2,21 +2,17 @@ package dte.amongus.shiptasks.list.wires;
 
 import java.util.Arrays;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang.WordUtils;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.text.WordUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import com.google.common.collect.Sets;
 
 import dte.amongus.AmongUs;
-import dte.amongus.cooldown.Cooldown;
 import dte.amongus.games.players.AUGamePlayer;
 import dte.amongus.shiptasks.types.inventory.TaskInventoryManager;
 import dte.amongus.utils.items.GlowEffect;
@@ -26,67 +22,47 @@ import dte.amongus.utils.java.objectholders.Pair;
 
 public class WiresInventoryManager extends TaskInventoryManager<WiresTask>
 {
-	private final Cooldown workCooldown;
-
-	private static final Integer[] 
+	private static final Integer[]
 			LEFT_SLOTS = {10, 19, 28, 37}, 
 			RIGHT_SLOTS = {16, 25, 34, 43};
-
-	private static final Material[] PROGRESSION_MATERIALS = {Material.GRAY_STAINED_GLASS_PANE, Material.WHITE_STAINED_GLASS_PANE, Material.BLACK_STAINED_GLASS_PANE};
+	
 	private static final Set<Material> WIRES_MATERIALS = Sets.newHashSet(Material.RED_STAINED_GLASS_PANE, Material.GREEN_STAINED_GLASS_PANE, Material.YELLOW_STAINED_GLASS_PANE, Material.PINK_STAINED_GLASS_PANE);
-
+	
 	static
 	{
-		//getRightWireSlot() uses a binary search
+		//getRightSlot() uses a binary search
 		Arrays.sort(LEFT_SLOTS);
 		Arrays.sort(RIGHT_SLOTS);
 	}
-
+	
 	public WiresInventoryManager(WiresTask wiresTask) 
 	{
-		super(wiresTask);
-
-		this.workCooldown = new Cooldown.CooldownBuilder("Wires")
-				.rejectWithMessage(ChatColor.RED + "You are still working on the previous wires.")
-				.whenOver((player, cooldown) ->
-				{
-					player.closeInventory();
-					String finishedWireName = this.task.getData(this.task.getGame().getPlayer(player), "Wire").map(String.class::cast).get();
-					player.sendMessage(ChatColor.WHITE + "You finished the " + ChatColor.GREEN + finishedWireName + ChatColor.WHITE + "Wires!");
-				})
-				.build();
+		super(wiresTask, "Fix The Wires");
 	}
 	
-	@Override
-	public boolean wasInvolvedAt(InventoryEvent event)
-	{
-		return event.getView().getTitle().equals("Task > Fix The Wires");
-	}
-
 	@Override
 	public Inventory createInventory(AUGamePlayer opener) 
 	{
 		Inventory inv = newInventoryBuilder(6)
-				.named("Fix The Wires")
 				.withWalls()
 				.build();
-		
+
 		Set<Material> wires = Sets.newHashSet(WIRES_MATERIALS);
 		Set<Integer> leftSlots = Sets.newHashSet(LEFT_SLOTS);
 		Set<Integer> rightSlots = Sets.newHashSet(RIGHT_SLOTS);
-		
-		for(int i = 1; i <= WiresTask.WIRES_AMOUNT; i++) 
+
+		for(int i = 1; i <= WiresTask.WIRES_AMOUNT; i++)
 		{
 			//create a new wire from random data
 			int leftSlot = RandomUtils.randomElement(leftSlots);
-			int rightSlot = getRightWireSlot(leftSlot);
+			int rightSlot = getRightSlot(leftSlot);
 			Material material = RandomUtils.randomElement(wires);
 
 			//set the wire at the left & right slots
 			ItemStack wire = new ItemBuilder(material, getWireName(material) + " Wire").createCopy();
 			inv.setItem(leftSlot, wire);
 			inv.setItem(rightSlot, wire);
-			
+
 			//remove the wire's data from the remaining data
 			leftSlots.remove(leftSlot);
 			rightSlots.remove(rightSlot);
@@ -102,48 +78,51 @@ public class WiresInventoryManager extends TaskInventoryManager<WiresTask>
 		
 		if(!WIRES_MATERIALS.contains(wire.getType()))
 			return;
-
+		
 		Player player = (Player) event.getWhoClicked();
 		AUGamePlayer gamePlayer = this.task.getGame().getPlayer(player);
 		
-		Pair<Integer, ItemStack> currentWireData = this.task.getCurrentWire(gamePlayer);
-		
+		Pair<Integer, ItemStack> currentWireData = this.task.getCurrentWire(gamePlayer).orElse(null);
+
 		if(currentWireData == null)
 		{
-			if(this.workCooldown.wasRejected(player))
+			if(!isLeftSlot(event.getRawSlot())) 
+			{
+				player.sendMessage(ChatColor.RED + "You have to click a Left wire first!");
 				return;
-			
+			}
 			GlowEffect.addGlow(wire);
 			this.task.setCurrentWire(gamePlayer, event.getRawSlot(), wire);
-			return;
 		}
-		if(event.getRawSlot() == getRightWireSlot(currentWireData.getFirst()) && wire.getType() == currentWireData.getSecond().getType())
+		else if(event.getRawSlot() == getRightSlot(currentWireData.getFirst()))
 		{
-			this.task.addProgression(gamePlayer, 25);
-			this.task.removeData(gamePlayer, "Start");
+			ItemStack rightWire = event.getInventory().getItem(getRightSlot(currentWireData.getFirst()));
+			GlowEffect.addGlow(rightWire);
 			
-			new WiresConnectionRunnable(event.getInventory(), currentWireData.getFirst()+1, event.getRawSlot()-1, PROGRESSION_MATERIALS).runTaskTimer(AmongUs.getInstance(), 0, 2 * 20);
-			GlowEffect.deleteGlow(currentWireData.getSecond());
-			this.workCooldown.put(player, TimeUnit.SECONDS, 10);
+			this.task.addProgression(gamePlayer, 25);
+			this.task.removeCurrentWire(gamePlayer);
+			
+			new WiresConnectionRunnable(event.getInventory(), currentWireData.getFirst()+1, event.getRawSlot()-1, Material.WHITE_STAINED_GLASS).runTaskTimer(AmongUs.getInstance(), 0, 10);
 		}
 	}
 	
-	private static int getRightWireSlot(int leftInvSlot) 
+	private static boolean isLeftSlot(int slot) 
 	{
-		int slot = Arrays.binarySearch(LEFT_SLOTS, leftInvSlot);
-		
+		return Arrays.binarySearch(LEFT_SLOTS, slot) > -1;
+	}
+	private static int getRightSlot(int leftSlot) 
+	{
+		int slot = Arrays.binarySearch(LEFT_SLOTS, leftSlot);
+
 		//get the parallel cell in the right slots array
 		return RIGHT_SLOTS[slot];
 	}
-	
 	private static String getWireName(Material wireMaterial) 
 	{
-		String colorName = wireMaterial.name().substring(0, wireMaterial.name().indexOf('_'));
+		String colorName = WordUtils.capitalizeFully(wireMaterial.name().substring(0, wireMaterial.name().indexOf('_')));
 
-		return getWireColor(wireMaterial) + WordUtils.capitalizeFully(colorName);
+		return getWireColor(wireMaterial) + colorName;
 	}
-
-	
 	private static ChatColor getWireColor(Material wireMaterial) 
 	{
 		if(wireMaterial == Material.PINK_STAINED_GLASS_PANE) 
--- src/dte/amongus/shiptasks/types/inventory/TaskInventoryManager.java
+++ src/dte/amongus/shiptasks/types/inventory/TaskInventoryManager.java
@@ -3,42 +3,45 @@ package dte.amongus.shiptasks.types.inventory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.ObjectUtils;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import dte.amongus.games.players.AUGamePlayer;
-import dte.amongus.shiptasks.ShipTask;
 import dte.amongus.utils.InventoryUtils;
 import dte.amongus.utils.items.ItemBuilder;
 
-public abstract class TaskInventoryManager<T extends ShipTask>
+public abstract class TaskInventoryManager<T extends InventoryTask<?>>
 {
 	protected final T task;
 	
-	public TaskInventoryManager(T task) 
+	private final String inventoryTitle;
+	
+	public TaskInventoryManager(T task, String inventoryTitle) 
 	{
 		this.task = task;
+		this.inventoryTitle = String.format(InventoryBuilder.TITLE_PATTERN, inventoryTitle);
 	}
 	public InventoryBuilder newInventoryBuilder(int lines) 
 	{
 		return new InventoryBuilder(lines);
 	}
-	
+	public boolean wasInvolvedAt(InventoryClickEvent event) 
+	{
+		return event.getView().getTitle().equals(this.inventoryTitle);
+	}
 	public abstract Inventory createInventory(AUGamePlayer opener);
-	public abstract boolean wasInvolvedAt(InventoryEvent event);
 	public abstract void onInventoryClick(InventoryClickEvent event); //change to return a boolean that indicates whether the player finished the task
 	
 	public class InventoryBuilder
 	{
 		private int lines;
-		private String customName;
 		
 		//walls data
 		private boolean buildWalls;
 		private ItemStack wallsItem;
+		
+		private static final String TITLE_PATTERN = "Task > %s";
 		
 		public InventoryBuilder(int lines) 
 		{
@@ -54,26 +57,14 @@ public abstract class TaskInventoryManager<T extends ShipTask>
 		{
 			return withWalls(new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE).named(ChatColor.BLACK + "-").createCopy());
 		}
-		public InventoryBuilder named(String customName) 
-		{
-			this.customName = customName;
-			return this;
-		}
 		public Inventory build()
 		{
-			Inventory inv = Bukkit.createInventory(null, 9 * this.lines, createTitle());
-
-			if(this.buildWalls) 
-				InventoryUtils.buildWalls(inv, this.wallsItem);
-
-			return inv;
-		}
-		
-		private String createTitle() 
-		{
-			String description = ObjectUtils.defaultIfNull(this.customName, TaskInventoryManager.this.task.getDescription());
+			Inventory inv = Bukkit.createInventory(null, 9 * this.lines, TaskInventoryManager.this.inventoryTitle);
 			
-			return String.format("Task > %s", description);
+			if(this.buildWalls)
+				InventoryUtils.buildWalls(inv, this.wallsItem);
+			
+			return inv;
 		}
 	}
 }
