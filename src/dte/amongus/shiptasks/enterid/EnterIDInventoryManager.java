8e3af784898d33b4f82847f79e019503b95866f9
David <darknight4123@gmail.com>
2021-05-06 00:49:50 +0300

    Completed the "Enter ID" Task

--- src/dte/amongus/shiptasks/enterid/EnterIDInventoryManager.java
+++ src/dte/amongus/shiptasks/enterid/EnterIDInventoryManager.java
@@ -2,71 +2,116 @@ package dte.amongus.shiptasks.enterid;
 
 import static dte.amongus.utils.ChatColorUtils.bold;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import dte.amongus.AmongUs;
 import dte.amongus.games.players.AUGamePlayer;
 import dte.amongus.shiptasks.inventory.TaskInventoryManager;
 import dte.amongus.utils.InventoryUtils;
 import dte.amongus.utils.items.GlowEffect;
 import dte.amongus.utils.items.ItemBuilder;
-import dte.amongus.utils.java.RandomUtils;
 
 public class EnterIDInventoryManager extends TaskInventoryManager<EnterIDTask>
 {
-	public EnterIDInventoryManager(EnterIDTask task)
+	private final Sound digitEnterSound;
+
+	private static final int 
+	ENTER_INDEX = 38,
+	PAPER_INDEX = 42;
+
+	public EnterIDInventoryManager(EnterIDTask task, Sound digitEnterSound)
 	{
 		super(task);
+
+		this.digitEnterSound = digitEnterSound;
 	}
 
 	@Override
 	public Inventory createInventory(AUGamePlayer opener)
 	{
-		Inventory inventory = new InventoryBuilder(6, "Your ID is " + generateRandomID()).build();
-		
-		//add the digits
-		for(int i = 10, digit = 1; i <= 16; i++, digit++)
-			inventory.setItem(i, createDigitItem(digit));
+		Inventory inventory = new InventoryBuilder(6, "Your ID is " + this.task.getPersonalID(opener).get()).build();
 
+		//add the digits
+		for(int digit = 1; digit <= 7; digit++) 
+		{
+			int i = digit + 9;
+			inventory.setItem(i, createDigitItem(digit));
+		}
+		inventory.setItem(4, createDigitItem(0));
 		inventory.setItem(21, createDigitItem(8));
 		inventory.setItem(23, createDigitItem(9));
-		
+
 		//add the enter button
 		inventory.setItem(37, InventoryBuilder.buildWall(Material.GREEN_STAINED_GLASS_PANE));
-		inventory.setItem(38, new ItemBuilder(Material.GREEN_TERRACOTTA, bold(ChatColor.GREEN) + "Enter ID").createCopy());
+		inventory.setItem(ENTER_INDEX, new ItemBuilder(Material.GREEN_TERRACOTTA, bold(ChatColor.GREEN) + "Enter ID").createCopy());
 		inventory.setItem(39, InventoryBuilder.buildWall(Material.GREEN_STAINED_GLASS_PANE));
-		
+
 		//add the current id paper
 		inventory.setItem(41, InventoryBuilder.buildWall(Material.GREEN_STAINED_GLASS_PANE));
-		inventory.setItem(42, new ItemBuilder(Material.PAPER, ChatColor.AQUA + "Current Code: Nothing").createCopy());
+		inventory.setItem(PAPER_INDEX, createPaperItem("Nothing"));
 		inventory.setItem(43, InventoryBuilder.buildWall(Material.GREEN_STAINED_GLASS_PANE));
-		
+
 		//decorate the remaining slots as walls
 		InventoryUtils.fillEmptySlots(inventory, InventoryBuilder.buildWall(Material.BLACK_STAINED_GLASS_PANE));
 
 		return inventory;
 	}
 
+	@SuppressWarnings("incomplete-switch")
 	@Override
 	public void onInventoryClick(InventoryClickEvent event) 
 	{
+		Player player = (Player) event.getWhoClicked();
+		AUGamePlayer gamePlayer = this.task.getGame().getPlayer(player);
 		ItemStack item = event.getCurrentItem();
-		
+
 		switch(item.getType()) 
 		{
 		case LIME_STAINED_GLASS_PANE:
-			int lastDigitIndex = InventoryUtils.firstSlotWhoseItem(event.getInventory(), GlowEffect::hasGlow);
 			
-			if(lastDigitIndex == -1) 
+			//glow the digit for 8 ticks
+			GlowEffect.addGlow(item);
+			Bukkit.getScheduler().runTaskLater(AmongUs.getInstance(), () -> GlowEffect.deleteGlow(item), 8);
+			
+			try 
 			{
-				GlowEffect.addGlow(item);
+				this.task.enterDigit(gamePlayer, getDigit(item));
+			}
+			catch(ArithmeticException exception) 
+			{
+				player.sendMessage(ChatColor.RED + "Too Many Digits!");
 				return;
 			}
+			
+			//update the new digit
+			event.getInventory().setItem(PAPER_INDEX, createPaperItem(this.task.getEnteredID(gamePlayer).get()));
+			
+			player.playSound(player.getLocation(), this.digitEnterSound, 1, 1);
+			break;
+			
+		case GREEN_TERRACOTTA:
+			Integer enteredID = this.task.getEnteredID(gamePlayer).orElse(null);
+
+			if(enteredID == null)
+			{
+				player.sendMessage(ChatColor.RED + "What's your Personal ID?");
+				return;
+			}
+			if(!this.task.getPersonalID(gamePlayer).get().equals(enteredID))
+			{
+				player.sendMessage(ChatColor.RED + "Invalid Personal ID!");
+				return;
+			}
+			player.sendMessage(ChatColor.GREEN + "Success - You were successfully identified.");
+			break;
 		}
-		
 	}
 
 	@Override
@@ -75,16 +120,29 @@ public class EnterIDInventoryManager extends TaskInventoryManager<EnterIDTask>
 		return testInventory(event, "Your ID is ");
 	}
 
-	//returns a 3-5 digits number
-	private static int generateRandomID() 
+	private static ItemStack createDigitItem(int digit)
 	{
-		return RandomUtils.randomInt(100, 99999, true, true);
+		int amount = digit == 0 ? 1 : digit;
+
+		return new ItemBuilder(Material.LIME_STAINED_GLASS_PANE, ChatColor.GREEN + String.valueOf(digit))
+				.amountOf(amount)
+				.createCopy();
+	}
+	
+	private static ItemStack createPaperItem(int id) 
+	{
+		return createPaperItem(String.valueOf(id));
 	}
 
-	private static ItemStack createDigitItem(int digit) 
+	private static ItemStack createPaperItem(String id)
 	{
-		return new ItemBuilder(Material.LIME_STAINED_GLASS_PANE, ChatColor.GREEN.toString() + digit)
-				.amountOf(digit)
-				.createCopy();
+		return new ItemBuilder(Material.PAPER, ChatColor.AQUA + "Entered ID: " + id).createCopy();
+	}
+
+	private static int getDigit(ItemStack digitItem)
+	{
+		String digitText = ChatColor.stripColor(digitItem.getItemMeta().getDisplayName());
+
+		return Integer.valueOf(digitText);
 	}
 }
--- src/dte/amongus/shiptasks/enterid/EnterIDTask.java
+++ src/dte/amongus/shiptasks/enterid/EnterIDTask.java
@@ -2,19 +2,23 @@ package dte.amongus.shiptasks.enterid;
 
 import java.util.Optional;
 
+import org.bukkit.Sound;
+
 import dte.amongus.games.AUGame;
 import dte.amongus.games.players.AUGamePlayer;
 import dte.amongus.shiptasks.SimpleShipTask;
 import dte.amongus.shiptasks.inventory.InventoryTask;
 import dte.amongus.shiptasks.type.TaskType;
+import dte.amongus.utils.java.NumberUtils;
+import dte.amongus.utils.java.RandomUtils;
 
 public class EnterIDTask extends SimpleShipTask implements InventoryTask<EnterIDInventoryManager>
 {
-	private final EnterIDInventoryManager inventoryManager = new EnterIDInventoryManager(this);
-	
+	private final EnterIDInventoryManager inventoryManager = new EnterIDInventoryManager(this, Sound.BLOCK_ANVIL_USE);
+
 	public EnterIDTask(AUGame game)
 	{
-		super("Enter ID", "Enter your personal ID", TaskType.COMMON, game);
+		super("Enter ID", "Enter your Personal ID", TaskType.COMMON, game);
 	}
 
 	@Override
@@ -23,25 +27,37 @@ public class EnterIDTask extends SimpleShipTask implements InventoryTask<EnterID
 		return this.inventoryManager;
 	}
 	
-	public void setID(AUGamePlayer gamePlayer, int digit) 
+	public Optional<Integer> getPersonalID(AUGamePlayer gamePlayer) 
 	{
-		
+		return getData(gamePlayer, "Personal ID").map(Integer.class::cast);
 	}
 	
-	public void addDigit(AUGamePlayer gamePlayer, int digit) 
+	public Optional<Integer> getEnteredID(AUGamePlayer gamePlayer)
 	{
-		Optional<String> playerID = getData(gamePlayer, "ID").map(String.class::cast);
+		return getData(gamePlayer, "Entered ID").map(Integer.class::cast);
+	}
+	
+	public void enterDigit(AUGamePlayer gamePlayer, int digit) throws ArithmeticException
+	{
+		int currentEnteredID = getOrPut(gamePlayer, "Entered ID", 0);
+
+		setData(gamePlayer, "Entered ID", NumberUtils.add(currentEnteredID, digit));
+	}
+	
+	@Override
+	public void onStart(AUGamePlayer gamePlayer) 
+	{
+		//set the player's personal ID as a 3-5 digits number
+		int randomID = RandomUtils.randomInt(100, 99999, true, true);
 		
-		if(!playerID.isPresent()) 
-		{
-			setData(gamePlayer, "ID", String.valueOf(digit));
-			return;
-		}
-		else 
-		{
-			String updatedID = playerID.get() + String.valueOf(digit);
-			
-			setData(gamePlayer, "ID", updatedID);
-		}
+		setData(gamePlayer, "Personal ID", randomID);
+	}
+	
+	@Override
+	public void onFinish(AUGamePlayer gamePlayer) 
+	{
+		InventoryTask.super.onFinish(gamePlayer);
+		
+		removeData(gamePlayer, "Personal ID");
 	}
 }
